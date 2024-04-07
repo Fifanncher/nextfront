2850ce4b00e32b4ea5e07674ee0123258dafc099
Крис <kriska_555@mail.ru>
2024-04-08 03:20:17 +0500

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 
 const API_URL = `https://api.master-pola.com/api/`;
+
 const baseQuery = axios.create({
-    baseURL: API_URL,
-    responseType: 'json',
-    // withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-    }
+  baseURL: API_URL,
+  responseType: 'json',
+  // withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
 });
 
 const API = {
-    post: (address, body) => baseQuery
-        .post(address, body)
-        .then(({data}) => data)
-        .catch(err => console.log(err)),
-    get: (address) => baseQuery
-        .get(address)
-        .then(({data}) => data)
-        .catch(err => console.log(err))
+  post: (address, body) => baseQuery
+    .post(address, body)
+    .then(({data}) => data)
+    .catch((err) => {}),
+  get: (address) => baseQuery
+    .get(address)
+    .then(({data}) => data)
+    .catch((err) => {})
 };
+
 export default API;
