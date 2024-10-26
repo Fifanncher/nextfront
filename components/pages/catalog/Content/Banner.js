fd97edbf3fcf62af48038c1b153e1f80b7ec9dca
kris <kriska_555@mail.ru>
2024-10-26 20:29:02 +0500

    banners

--- components/pages/catalog/Content/Banner.js
+++ components/pages/catalog/Content/Banner.js
@@ -58,7 +58,7 @@ const items = [
     category: 'quartzvinyl_zamkovay'
   },
   {
-    background: 'https://master-pola.com/dashboard/uploads/doors_07625c5346.png',
+    background: 'https://master-pola.com/dashboard/uploads/Artboard_1_cbc63f05b8.png',
     mobileBackground: 'https://master-pola.com/dashboard/uploads/mob_dveri_f4720179dd.png'
   },
   {
